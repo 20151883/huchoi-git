7011f9c3c4b7f6de65d7891e6e26a0580cb02e9f
20151883 <nyz1058@nate.com>
2021-01-18 22:03:19 +0900

    origin/master

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -9,4 +9,5 @@ int ft_strlen(char *str);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strchr(char *s, int c);
 int     get_next_line(int fd, char **line);
+char	*ft_strrchr(char *s, int c);
 #endif
