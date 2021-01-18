82b1dea4b4122efd9d89aed5e65d3a7e4130fe51
20151883 <nyz1058@nate.com>
2021-01-18 15:45:04 +0900

    origin/master

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -3,9 +3,10 @@
 
 #include <unistd.h>
 #include <stdlib.h>
-
+#include <fcntl.h>
 char	*ft_strdup(char *src);
 int ft_strlen(char *str);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strchr(char *s, int c);
+int     get_next_line(int fd, char **line);
 #endif
---
+++ get_next_line/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "get_next_line.h"
+
+int main(void)
+{
+    char *line;
+
+    int fd = open("test.txt", O_RDONLY)
+    get_next_line(fd, &line);
+    printf("%s\n" line);
+    return (0);
+}
