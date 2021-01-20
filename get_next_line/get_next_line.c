09d5d48d9d5997a3a364f8b2aa4bd9c36a619fd2
Hunjin Choi <huchoi@c4r3s6.42seoul.kr>
2021-01-20 15:13:25 +0900

    origin/master

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,109 +6,105 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 10:43:14 by marvin            #+#    #+#             */
-/*   Updated: 2021/01/19 20:41:53 by huchoi           ###   ########.fr       */
+/*   Updated: 2021/01/20 15:11:07 by huchoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-void free_back_buf(char *p1, char *p2)
+
+void	free_back_buf(char *p1, char *p2)
 {
-    if (p1 != 0)
-        free(p1);
-    if (p2 != 0)
-        free(p2);
+	if (p1 != 0)
+		free(p1);
+	if (p2 != 0)
+		free(p2);
 }
 
-void    renewer_backup(char **line, char **p_backup, char **p_buf, char *p)
+int		renewer_backup(char **line, char **p_backup, char **p_buf, char *p)
 {
-    char *temp;
+	char *temp;
 
-    *p = '\0';
-    *line = ft_strdup(*p_backup);
-    temp = ft_strdup(++p);
-    free_back_buf(*p_backup, *p_buf);
-    *p_backup = temp;
+	*p = '\0';
+	*line = ft_strdup(*p_backup);
+	temp = ft_strdup(++p);
+	free_back_buf(*p_backup, *p_buf);
+	*p_backup = temp;
+	return (1);
 }
 
-int case_ret_zero(char **line, char **p_backup, char **p_buf)
+int		case_ret_zero(char **line, char **p_backup, char **p_buf)
 {
-    char *p;
+	char *p;
 
-    if (*p_backup != NULL)
-    {
-        p = ft_strchr(*p_backup, '\n');
-        if (p != 0)
-        {
-            renewer_backup(line, p_backup, p_buf, p);
-            return (1);
-         }
-        *line = (char *)ft_strdup(*p_backup);
-        free_back_buf(*p_backup, *p_buf);
-        *p_backup = NULL;
-        return (0);//return zero or one ??!!!???!!!
-    }
-    else
-    {
-        *line = ft_strdup("");
+	if (*p_backup != NULL)
+	{
+		p = ft_strchr(*p_backup, '\n');
+		if (p != 0)
+		{
+			renewer_backup(line, p_backup, p_buf, p);
+			return (1);
+		}
+		*line = (char *)ft_strdup(*p_backup);
+		free_back_buf(*p_backup, *p_buf);
+		*p_backup = NULL;
+		return (0);
+	}
+	else
+	{
+		*line = ft_strdup("");
 		free(*p_buf);
-        return (0);//return zero is coreect...! because main process the free(line)!!
-    }
+		return (0);
+	}
 }
 
-int case_ret_non_zero(char **line, char **p_backup, char **p_buf)
+int		case_ret_non_zero(char **line, char **p_backup, char **p_buf)
 {
-    char *p;
-    char *temp;
+	char *p;
+	char *temp;
 
-    if (*p_backup != NULL)
-    {
-        temp = ft_strjoin(*p_backup, *p_buf);
-        free(*p_backup);
-        *p_backup = temp;
+	if (*p_backup != NULL)
+	{
+		temp = ft_strjoin(*p_backup, *p_buf);
+		free(*p_backup);
+		*p_backup = temp;
 		p = ft_strchr(*p_backup, '\n');
-        if (p == (char *)NULL)
-            return (42);
-        else
-        {
-           renewer_backup(line, p_backup, p_buf, p);
-           return (1);
-        }
-    }
-    else
-    {
-        *p_backup = ft_strdup(*p_buf);
+		if (p == (char *)NULL)
+			return (42);
+		else
+			return (renewer_backup(line, p_backup, p_buf, p));
+	}
+	else
+	{
+		*p_backup = ft_strdup(*p_buf);
 		p = ft_strchr(*p_backup, '\n');
-        if (p == (char *)NULL)
-            return (42);
-        else
-        {
-            renewer_backup(line, p_backup, p_buf, p);
-        	return (1);
-        }
-    }
+		if (p == (char *)NULL)
+			return (42);
+		else
+			return (renewer_backup(line, p_backup, p_buf, p));
+	}
 }
 
-int get_next_line(int fd, char **line)
+int		get_next_line(int fd, char **line)
 {
-    static char *backup;
-    char *buf;
-    int ret;
+	static char *backup;
+	char		*buf;
+	int			ret;
 
 	if (BUFFER_SIZE == 0 || BUFFER_SIZE == -1 || fd < 0 || line == 0)
 		return (-1);
-    buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-    while((ret = read(fd, buf, BUFFER_SIZE)) >= 0)
-    {
-        buf[ret] = '\0';//NULL terminated
-        if (ret == 0)//EOF를 만났을때라고 단정지을수있나? 뭔소리지...???
-            return (case_ret_zero(line, &backup, &buf));
-        else if (0 < ret && ret <= BUFFER_SIZE)
-        {
+	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	while ((ret = read(fd, buf, BUFFER_SIZE)) >= 0)
+	{
+		buf[ret] = '\0';
+		if (ret == 0)
+			return (case_ret_zero(line, &backup, &buf));
+		else if (0 < ret && ret <= BUFFER_SIZE)
+		{
 			ret = case_ret_non_zero(line, &backup, &buf);
 			if (ret == 42)
 				continue;
-        	return (ret);
-        }
-    }
-    return (-1);
+			return (ret);
+		}
+	}
+	return (-1);
 }
