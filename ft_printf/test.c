bc96ca393ea6010eec1fb260864127a605da9644
Hunjin Choi <huchoi@c4r3s6.42seoul.kr>
2021-01-23 14:35:08 +0900

    origin/master

--- ft_printf/test.c
+++ ft_printf/test.c
@@ -6,12 +6,13 @@ int main(void)
 	int num = 42;
 	char arr[7];
 
-	strcpy_s(arr,7, "string");
-	printf("%-08.5d\n",num);
-	printf("%0-5.10s\n", arr);
+	strcpy(arr, "string");
+	printf("%08.5d\n",num);
+//	printf("%05.10s\n", arr);
     printf("%-3.5s\n",arr);
     printf("%-6.3s\n",arr);
     printf("%-8.6s\n",arr);
-    printf("%s-8.8s\n",arr);
+    printf("%-8.8s\n",arr);
 	return(0);
 }
+
