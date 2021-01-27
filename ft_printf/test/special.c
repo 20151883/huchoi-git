c321d583f9a62145ae5ea2e0397f9f71b1e8ef55
20151883 <71176033+20151883@users.noreply.github.com>
2021-01-27 13:16:07 +0900

    Update special.c

--- ft_printf/test/special.c
+++ ft_printf/test/special.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	printf("[%-010.5%]");//in genalral case flag,  -,0 can not co-work(it means error!!!)alike c,s,p can not co-work with 0 flag(this is also error
+	printf("[%-010.5%]\n");//in genalral case flag,  -,0 can not co-work(it means error!!!)alike c,s,p can not co-work with 0 flag(this is also error
 	printf("[%%         ]");         
 	return (0);
 }
