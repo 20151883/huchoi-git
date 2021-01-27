84184d2a877b5ddb56260a5af738d86472e7a05b
20151883 <nyz1058@nate.com>
2021-01-27 15:35:23 +0900

    origin/master

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -15,36 +15,81 @@ int ft_printf(const char* str, ...)
 	//이때, 참조만 하는거기 때문에 포인터의 변화, data의 변화등등은 없을것이다.
 	//그러니까 char *parr = va_args(ap, char *) 이렇게 쓰는게 맞을것이다.
 	char* arr;
+	char *temp = ft_calloc(999999);//메모리크기 근거가 부족하다. 1로 바꿧다 이떄는 근거가 있다.
 	char ch;
 	int	num;
-
-	while (*str)
+	int count;
+	char **final = malloc(sizeof(char *) * 6);
+	char *buf = 0;
+	char *two = ft_calloc(2);
+	int i = 0;
+	int j;
+	while(str[i] != '\0')
 	{
-		if (strncmp(str, "%s",2) == 0)
+		if(str[i] != '%')
 		{
-			tmp = va_arg(ap, char*);
-			while (*tmp)
-				putchar(*tmp++);
-			str = str + 2;
+			ft_putchar_fd(str[i++], 1);
+		}	continue;
+		i++;
+		j = 0;
+		//temp 의 모든 요소가0으로 초기화된상태...? 그건 보장안함.
+		//buf는 항상 0인 상태로 루프 진입한다
+		int case;
+		case = what_case(str[i]);
+		while(ft_strcmp(str[i], "cspdiuxX%") == 0)
+		{
+			if (case != what_case(str[i]))
+			{}
+				arr[case] = ft_strdup(temp);
+				case = what_case(str[i]);
+				memset(temp, 0, ft_strlen(temp + 1));//ft_strlen(temp)해줘도 될거같긴한데.
+			}
+			if(case == 3)
+				i++;//사소하지만 중요한 부분
+			if (str[i] != '*')
+			{	
+				two[0] = str[i];
+				temp = ft_strjoin(temp, two);
+			}
+			else
+			{
+				buf = ft_itoa(va_arg(ap, int));
+				ft_strjoin(temp, buf)
+				free(buf);
+				buf = 0;
+			}
 		}
-		else if (strncmp(str, "%c", 2) == 0)
+		arr[case] = ft_strdup(temp);
+		memset(temp, 0, ft_strlen(temp));
+
+		the_type = str[i];
+		//type_to_data(&temp, va_arg(ap, ))//tpye_to_Data(char **p_temp, void data)
+		if(the_type == 'c')
 		{
-			ch = va_arg(ap, char);
-			putchar(ch)
-				str = str + 2
+			two[0] = (char)va_arg(ap, int);
+			ft_strjoin(temp, two);
 		}
-		else if (strncmp(str, "%d", 2) == 0)
+		if(the_type == 's')
 		{
-			num = va_arg(ap, int);
-			ft_putnbr(num);//to stdout...
-			str = str + 2;
+			ft_strjoin(temp, va_arg(ap, char *));
+		}
+		if(the_type == 'p')
+		{
+			printf("pointer...!!!")
+		}
+		if(the_type == '%')
+		{
+			two[0] = '%';
+			ft_strjoin(temp, two);
 		}
 		else
 		{
-			putchar(*str);
-			str++;
+			arr[5] = ft_itoa(va_arg(ap, int));//일단 가변인자쪽은 무조건 10진수 정수이다???,.,말이 이상하긴한데 암튼 그럼...
+			//그래서 일단 10진수로 해석하고 그 이후에 저장해놓은 the_tyep에 맞게 변환하면 되겠다. 이 작업은 make_string에서 하겠다.
 		}
+		ft_putstr_fd(make_string(the_type,temp), 1)//출력할 문자열을 멩글어 주는 함수//여기서 여러가지 분기가 일어나겠다.
+		temp에서 맨 뒤부터 보겠.
+		i++;//사소하지만 중요한 부분
 	}
-
-
+	return (count);
 }
---
+++ ft_printf/protect_star.c
@@ -0,0 +1,4 @@
+#include "libft.h"
+int j = 1;
+while(str[[i + j] is not )
+void inte_star(va_list *p_ap, arr *str)
