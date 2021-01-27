5899ff64e6c6ed3b5f42e6c5b119a5f3745e410a
20151883 <nyz1058@nate.com>
2021-01-27 15:49:24 +0900

    origin/master

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -2,10 +2,28 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+#include "libft.h"
 //va_strat
 //va_arg
 //va_copy
 //va_end
+int what_case(char c)
+{
+    char *arr;
+
+    arr = malloc(10);
+    ft_strcpy(arr, "cspdiuxX%");
+    if (c == '-' || c == '0')
+        return (1);
+    if('1' < c && c < '9')
+        return (2)
+    if(c == '.')
+        return (3);
+    if (ft_strchr(arr, c) != 0)
+        return (5);
+    return (-1);
+}
+
 int ft_printf(const char* str, ...)
 {
 	va_list ap;//va_list *ap is not correct
@@ -15,15 +33,16 @@ int ft_printf(const char* str, ...)
 	//이때, 참조만 하는거기 때문에 포인터의 변화, data의 변화등등은 없을것이다.
 	//그러니까 char *parr = va_args(ap, char *) 이렇게 쓰는게 맞을것이다.
 	char* arr;
-	char *temp = ft_calloc(999999);//메모리크기 근거가 부족하다. 1로 바꿧다 이떄는 근거가 있다.
+	char *temp = ft_calloc(999999,1);//메모리크기 근거가 부족하다. 1로 바꿧다 이떄는 근거가 있다.
 	char ch;
 	int	num;
 	int count;
 	char **final = malloc(sizeof(char *) * 6);
 	char *buf = 0;
-	char *two = ft_calloc(2);
+	char *two = ft_calloc(2, 1);
 	int i = 0;
 	int j;
+	char the_type;
 	while(str[i] != '\0')
 	{
 		if(str[i] != '%')
@@ -36,11 +55,11 @@ int ft_printf(const char* str, ...)
 		//buf는 항상 0인 상태로 루프 진입한다
 		int case;
 		case = what_case(str[i]);
-		while(ft_strcmp(str[i], "cspdiuxX%") == 0)
+		while(ft_strncmp(str[i], "cspdiuxX%", 10) == 0)
 		{
 			if (case != what_case(str[i]))
-			{}
-				arr[case] = ft_strdup(temp);
+			{
+				final[case] = ft_strdup(temp);
 				case = what_case(str[i]);
 				memset(temp, 0, ft_strlen(temp + 1));//ft_strlen(temp)해줘도 될거같긴한데.
 			}
@@ -54,12 +73,12 @@ int ft_printf(const char* str, ...)
 			else
 			{
 				buf = ft_itoa(va_arg(ap, int));
-				ft_strjoin(temp, buf)
+				ft_strjoin(temp, buf);
 				free(buf);
 				buf = 0;
 			}
 		}
-		arr[case] = ft_strdup(temp);
+		final[case] = ft_strdup(temp);
 		memset(temp, 0, ft_strlen(temp));
 
 		the_type = str[i];
@@ -71,7 +90,7 @@ int ft_printf(const char* str, ...)
 		}
 		if(the_type == 's')
 		{
-			ft_strjoin(temp, va_arg(ap, char *));
+			temp = ft_strjoin(temp, va_arg(ap, char *));
 		}
 		if(the_type == 'p')
 		{
@@ -84,9 +103,10 @@ int ft_printf(const char* str, ...)
 		}
 		else
 		{
-			arr[5] = ft_itoa(va_arg(ap, int));//일단 가변인자쪽은 무조건 10진수 정수이다???,.,말이 이상하긴한데 암튼 그럼...
+			temp = ft_itoa(va_arg(ap, int));//일단 가변인자쪽은 무조건 10진수 정수이다???,.,말이 이상하긴한데 암튼 그럼...
 			//그래서 일단 10진수로 해석하고 그 이후에 저장해놓은 the_tyep에 맞게 변환하면 되겠다. 이 작업은 make_string에서 하겠다.
 		}
+		final[5] = ft_strdup(temp);
 		ft_putstr_fd(make_string(the_type,temp), 1)//출력할 문자열을 멩글어 주는 함수//여기서 여러가지 분기가 일어나겠다.
 		temp에서 맨 뒤부터 보겠.
 		i++;//사소하지만 중요한 부분
