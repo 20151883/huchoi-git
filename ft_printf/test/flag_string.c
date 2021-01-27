e42d04efb11c49ff9ec3ed3f908a5f14e39b5c69
20151883 <71176033+20151883@users.noreply.github.com>
2021-01-27 16:03:44 +0900

    Update flag_string.c

--- ft_printf/test/flag_string.c
+++ ft_printf/test/flag_string.c
@@ -3,12 +3,12 @@
 int main(void)
 {
 	printf("[%-.3s]","12345");
-	printf("[123]\n");
+	printf("[]\n");
 	printf("[%-4.3s]","12345");
-	printf("[123 ]\n");
+	printf("[]\n");
 	printf("[%-3.6s]","12345");
-	printf("[12345]\n");
+	printf("[]\n");
 	printf("[%-7.6s]","12345");
-	printf("[12345  ]\n");
+	printf("[]\n");
 	return (0);
 }
