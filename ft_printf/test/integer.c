f9870fa5200ef64ed64f4bdfed46cccda84fde47
20151883 <71176033+20151883@users.noreply.github.com>
2021-01-27 16:21:26 +0900

    Update integer.c

--- ft_printf/test/integer.c
+++ ft_printf/test/integer.c
@@ -3,12 +3,12 @@
 int main(void)
 {
 	printf("[%10.7d]",12345);
-	printf("[]\n");
+	printf("[0000012345]\n");
 	printf("[%5.7d]",12345);
-	printf("[]\n");
+	printf("[12345]\n");
 	printf("[%7.3d]",12345);
-	printf("[]\n");
+	printf("[  12345]\n");
 	printf("[%4.3d]",12345);
-	printf("[]\n");
+	printf("[12345]\n");
 	return (0);
 }
