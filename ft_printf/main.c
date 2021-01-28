3c83d7ba0a75f46e8cd1930440f48852a3273b24
Hunjin Choi <huchoi@c2r1s8.42seoul.kr>
2021-01-28 20:03:22 +0900

    origin/master

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -4,5 +4,17 @@ int main(void)
 {
 	ft_printf("[%-10c]\n",'z');
 	printf("[%-10.c]\n",'z');
+	ft_printf("[%s]\n","is it work?");
+	printf("[%s]\n","is it sork?");
+	ft_printf("[%10.5s]\n","is it work?");
+	printf("[%10.5s]\n","is it sork?");
+	ft_printf("[%-10.5s]\n","is it work?");
+	printf("[%-10.5s]\n","is it sork?");
+	ft_printf("[%-10.20s]\n","is it work?");
+	printf("[%-10.20s]\n","is it sork?");
+	ft_printf("[%2.5s]\n","is it work?");
+	printf("[%2.5s]\n","is it sork?");
+	ft_printf("[%30.20s]\n","is it work?");
+	printf("[%30.20s]\n","is it sork?");
 	return (0);
 }
