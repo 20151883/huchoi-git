c6474d21585dd2133cf5df03c868bf81287d3b88
20151883 <nyz1058@nate.com>
2021-01-28 15:06:26 +0900

    origin/master

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -103,19 +103,19 @@ int ft_printf(const char* str, ...)
 		}
 		final[my_case] = ft_strdup(temp);
 		my_case++;
-		while (my_case <= 5)
+		while (my_case < 5)
 		{
 			final[my_case] = ft_strdup("");
 			my_case++;
 		}
-		ft_memset(temp, '\0', ft_strlen(temp));
+		ft_memset(temp, '\0', ft_strlen(temp) + 1);
 		*temp = '\0';
 		the_type = str[i];
 		//type_to_data(&temp, va_arg(ap, ))//tpye_to_Data(char **p_temp, void data)
 		if(the_type == 'c')
 		{
 			two[0] = (char)va_arg(ap, int);
-			ft_strjoin(temp, two);
+			temp = ft_strjoin(temp, two);
 		}
 		if(the_type == 's')
 		{
@@ -128,14 +128,14 @@ int ft_printf(const char* str, ...)
 		if(the_type == '%')
 		{
 			two[0] = '%';
-			ft_strjoin(temp, two);
+			temp = ft_strjoin(temp, two);
 		}
 		else
 		{
 			temp = ft_itoa(va_arg(ap, int));//일단 가변인자쪽은 무조건 10진수 정수이다???,.,말이 이상하긴한데 암튼 그럼...
 			//그래서 일단 10진수로 해석하고 그 이후에 저장해놓은 the_tyep에 맞게 변환하면 되겠다. 이 작업은 make_string에서 하겠다.
 		}
-		//final[5] = ft_strdup(temp);
+		final[5] = ft_strdup(temp);
 
 		printf("%s\n%s\n%s\n%s\n\n",final[1], final[2], final[3], final[5]);
 		//ft_putstr_fd(make_string(the_type,temp), 1);//출력할 문자열을 멩글어 주는 함수//여기서 여러가지 분기가 일어나겠다.
