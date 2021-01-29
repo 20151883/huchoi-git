b3bd28019cc4ae0d7acd6c2f45d21ee0839c1c64
Hunjin Choi <huchoi@c2r1s8.42seoul.kr>
2021-01-29 10:50:24 +0900

    origin/master

--- ft_printf/test/special_test.c
+++ ft_printf/test/special_test.c
@@ -2,7 +2,17 @@
 
 int main(void)
 {
-	printf("[%-010.5%]\n");
-	printf("[%%         ]");    
+	printf("[%10.5%]\n");
+	printf("[         %%]\n");
+	printf("[%010%]\n");
+	printf("[000000000%%]\n");
+	printf("[%010.5%]\n");
+	printf("[000000000%%]\n");
+	printf("[%.5%]\n");
+	printf("[%%]\n");
+	printf("[%-0.100%]\n");
+	printf("[%%]\n");
+	printf("[%-5%]\n");
+	printf("[%%    ]\n");
 	return (0);
 }
