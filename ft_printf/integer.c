eaa17dfac72641e00482a9bdb861f6a5e00d154d
20151883 <nyz1058@nate.com>
2021-02-03 15:54:23 +0900

    origin/master

--- ft_printf/integer.c
+++ ft_printf/integer.c
@@ -84,10 +84,6 @@ char *case_i(t_node *p_node, char **line)
         else
             plus = 0;
     }
-    if (*line[3] != '\0' && size < atoi(line[3]))
-        plus = atoi(line[3]) - size;
-    else
-        plus = 0;
     lenth = size + plus;
     if (p_node->is_precision == 1)
         check_zero(line, &size, &plus, &lenth);
