a22c7f81dbea198d7998416680f347518fa9ed21
20151883 <nyz1058@nate.com>
2021-02-03 18:07:50 +0900

    origin/master

--- ft_printf/not_integer.c
+++ ft_printf/not_integer.c
@@ -46,11 +46,11 @@ char *case_s(t_node *p_node, char **line, char**p_temp)
 
 	i = 0;
 	size = ft_strlen(line[5] );
-	if ((0 < ft_atoi(line[3])) && (ft_atoi(line[3]) <= size))
+	if ((0 < ft_atoi(line[3])) && (ft_atoi(line[3]) < size))
 	{
 		size = ft_atoi(line[3]);
 	}
-	printf("test : %d", size);
+	printf("atoi(line[3] = %d   ft_strlen(line[5] = %d", ft_atoi(line[3]), ft_atoi(line[5]));
 	lenth = size;
 	if (lenth < ft_atoi(line[2]) && *line[2] != '\0')
 	{
