854a0b14b38162f6a3e80ffc40a0504ca69ce5d5
20151883 <nyz1058@nate.com>
2021-02-03 20:18:20 +0900

    origin/master

--- ft_printf/not_integer.c
+++ ft_printf/not_integer.c
@@ -90,7 +90,7 @@ char *case_s(t_node *p_node, char **line, char**p_temp)
 	}
 	else
 		ft_strlcpy(&ret[lenth - size], line[5], size + 1);
-	char ch = 0
+	char ch = 0;
 	if (size == 0)
 		ft_putchar_fd(ch, 1);
     return (ret);
