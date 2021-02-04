bcfb821a3ae85ff3cbdde52ec306ffe022fe9082
20151883 <nyz1058@nate.com>
2021-02-04 20:39:37 +0900

    origin/master

--- ft_printf/includes/ft_printf.h
+++ ft_printf/includes/ft_printf.h
@@ -39,7 +39,7 @@ int complete_final(char **line, int *p_my_case, char **p_temp);
 int flag_precise(char **line, char **p_temp,char *str, int *p_i, t_node *p_node);
 int case_changed(char **line, char **p_temp, int *p_my_case);
 int star(char **line, char **p_temp, char *buf, int my_case, t_node *p_node);
-int not_star(char **line, char *str, char **p_temp, int *p_i, t_node *p_node);
+int not_star(char **line, char *str, char **p_temp, t_node *p_node);
 int free_ret_zero(char **p, char *s1, char *s2, int my_case);
 char *precision_int(char **line, int plus, int size, int lenth);
 char *precision_unsigned(char **line, int plus, int size, int lenth);
