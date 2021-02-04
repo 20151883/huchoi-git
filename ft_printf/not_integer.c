4e53c04a1dc8bad26937c3d221c620e042b192fc
20151883 <nyz1058@nate.com>
2021-02-04 21:50:20 +0900

    origin/master

--- ft_printf/not_integer.c
+++ ft_printf/not_integer.c
@@ -64,56 +64,14 @@ char *case_s(t_node *p_node, char **line, char**p_temp)
 	int i;
 
 	i = 0;
-	/*size = ft_strlen(line[5] );
-	if ((0 < ft_atoi(line[3])) && (ft_atoi(line[3]) < size))
-	{
-		size = ft_atoi(line[3]);
-	}
-	//printf("atoi(line[3] = %d   ft_strlen(line[5] = %d", ft_atoi(line[3]), ft_atoi(line[5]));
-	lenth = size;
-	if (lenth < atoi_positive(line[2]) && *line[2] != '\0')
-	{
-		lenth = atoi_positive(line[2]);
-	}
-	else
-	{
-		lenth = lenth + 0;
-	}//[0] ~ [size] but  at [size] NULL ...so [0] ~ [size-1] ///[0]start or [size-lenth]start
-	if (ft_atoi(line[3]) == 0 && p_node->is_precision == 1)
-	{
-		if (ft_atoi(line[3]) == 0)
-		{
-			lenth = 0;
-			//lenth = 1???
-			size = 0;
-			//ft_putchar_fd(' ', 1);
-		}
-		if ((atoi_positive(line[2]) > 0))// && ft_strchr(line[2], '-')) //&& *line[1] == '\0')
-		{
-			p_node->count += atoi_positive(line[2]);
-			while (size++ < atoi_positive(line[2]))
-				ft_putchar_fd(' ', 1);
-			char *test;
-			test = ft_calloc(1,1);
-			return (test);
-		}
-	}*/
 	if (compute_lenth(p_node, line, &size, &lenth) == 0)
 		return (ft_strdup(""));
 	if (0 == (ret = malloc(sizeof(char) * (lenth + 1))))
 		free_ret_zero(line, *p_temp, 0, 6);
 	ret[lenth] = '\0';//needed...!!!
 	(p_node->count)+=lenth;
-	if(ft_strchr(line[1], '0') != 0)
-	{
-		while (i < lenth)
-			ret[i++] = ' ';
-	}	//return (NULL);//ssh printf는 출력은 시키던데...???
-	else
-	{
-		while(i < lenth)
-			ret[i++] = ' ';
-	}
+	while (i < lenth)
+		ret[i++] = ' ';
 	if (ft_strchr(line[1], '-') != 0)
 	{
 		ft_strlcpy(&ret[0], line[5], size + 1);
