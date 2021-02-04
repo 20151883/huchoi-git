2a1da3ad936f74d9bb84629ffd3ac8f645f9e250
20151883 <nyz1058@nate.com>
2021-02-04 14:43:25 +0900

    origin/master

--- ft_printf/not_integer.c
+++ ft_printf/not_integer.c
@@ -8,7 +8,10 @@ char *case_c(t_node *p_node, char **line, char**p_temp)//dot procedure is needed
 
 	if (ft_strncmp(line[5], "(null)", 10) == 0)
 	{
-			lenth = (1 > atoi_positive(line[2]) ? 1 : atoi_positive(line[2]));
+			if (atoi_positive(line[2]) > 1)
+				lenth = atoi_positive(line[2]);
+			else
+				lenth = 1;
 			int i = 0;
 			if (ft_strchr(line[1], '-') != 0)
 			{
