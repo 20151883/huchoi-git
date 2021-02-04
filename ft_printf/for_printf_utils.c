eab6184c5a8b1478bce6ec8c733f8557372834ac
20151883 <nyz1058@nate.com>
2021-02-04 14:55:36 +0900

    origin/master

--- ft_printf/for_printf_utils.c
+++ ft_printf/for_printf_utils.c
@@ -139,5 +139,6 @@ int real_complete_final(t_node *p_node, char **line, char**p_temp)
     p_node->is_precision = 0;
     p_node->buf = 0;
     p_node->two_star = 0;
+    p_node->value = 0;
     return (1);
 }
