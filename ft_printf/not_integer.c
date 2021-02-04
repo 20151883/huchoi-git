00d9bcd7dd06fbfac72f7b39901eae8c876ee563
20151883 <nyz1058@nate.com>
2021-02-04 10:07:59 +0900

    origin/master

--- ft_printf/not_integer.c
+++ ft_printf/not_integer.c
@@ -35,7 +35,8 @@ char *case_p(t_node *p_node, char **line, char**p_temp)
 	int plus;
 	char *save;
 
-	save = ft_strchr(&line[5][2], '0') + 1;
+	save = *line[5];
+	save+=2;
 	while (*save != '0')
 		save++;//done
 	size = ft_strlen(save) + 2;//0x//only number of character
@@ -45,10 +46,11 @@ char *case_p(t_node *p_node, char **line, char**p_temp)
 		lenth = size;
 	ret = (char *)ft_calloc(sizeof(char), (lenth + 1));
 	ft_memset(ret, ' ', lenth);
-	if (ft_strchr(line[1], '-'))
+	if (ft_strchr(line[1], '-'))//0 flag is not used...?
 		ft_strlcpy(ret, line[5], size + 1);
 	else
 		ft_strlcpy(&ret[lenth - size], line[5], size + 1);
+	printf("\nTEST : ret\n");
     return (ret);
 }
 
