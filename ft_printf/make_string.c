48a8e3392f49c7d238605e3b15486cf7e3b3cb62
20151883 <nyz1058@nate.com>
2021-02-05 14:33:19 +0900

    origin/master

--- ft_printf/make_string.c
+++ ft_printf/make_string.c
@@ -3,11 +3,11 @@
 char *make_string(t_node *p_node, char **final, char **p_temp, char the_type)//출력해야할 최종적 문자열을 만들어서 반환한다 (space,0채우기 &우측좌측정렬 완료된 문자열)
 {
     if (the_type == 'c')
-        return (case_c(p_node, final, p_temp));
+        return (case_c(p_node, final));
     else if (the_type == 's')
         return (case_s(p_node, final, p_temp));
     else if (the_type == 'p')
-        return (case_p(p_node, final, p_temp));
+        return (case_p(p_node, final));
     else if (the_type == '%')
         return (case_percent(p_node, final, p_temp));
     else if (the_type == 'd')
