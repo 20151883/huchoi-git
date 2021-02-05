33cb934e8d5456169980231fa10fe5a0c5da1d58
20151883 <nyz1058@nate.com>
2021-02-05 14:56:13 +0900

    origin/master

--- ft_printf/ft_printf_ver6.c
+++ ft_printf/ft_printf_ver6.c
@@ -1,19 +1,19 @@
 #include "./includes/ft_printf.h"
 
 va_list ap;
-int 	what_my_case(char c, int now_case)
+int		what_my_case(char c, int now_case)
 {
 	if (c == '-' || c == '0')
 	{
 		if (now_case <= 1)
 			return (1);
 	}
-	if(('0' <= c && c <= '9') || c == '*')
+	if (('0' <= c && c <= '9') || c == '*')
 	{
 		if ((now_case <= 2))
 			return (2);
 	}
-	if(('0' <= c && c <= '9') || c == '.' || c == '*')
+	if (('0' <= c && c <= '9') || c == '.' || c == '*')
 	{
 		if (now_case <= 3)
 			return (3);
@@ -21,9 +21,9 @@ int 	what_my_case(char c, int now_case)
 	if (ft_strchr("cspdiuxX%%", c) != 0)
 		return (5);
 	return (-1);
-}	
+}
 
-void 	init_node(t_node* p_node, const char *str)
+void	init_node(t_node *p_node, const char *str)
 {
 	p_node->count = 0;
 	p_node->idx = 0;
@@ -34,7 +34,7 @@ void 	init_node(t_node* p_node, const char *str)
 	p_node->two_star = 0;
 }
 
-int 	ft_printf(const char* str, ...)
+int		ft_printf(const char *str, ...)
 {
 	char	*temp;
 	char	*final[6];
@@ -44,16 +44,16 @@ int 	ft_printf(const char* str, ...)
 	va_start(ap, str);
 	while (str[node.idx] != '\0')
 	{
-		if(str[node.idx++] != '%')
+		if (str[node.idx++] != '%')
 		{
 			my_putchar_fd(&node);
 			continue;
 		}
 		if (0 == (temp = ft_calloc(1, sizeof(char))))
-			return (free_ret_zero(NULL, temp, NULL,node.my_case));
+			return (free_ret_zero(NULL, temp, NULL, node.my_case));
 		while (ft_strchr("cspdiuxX%%", str[node.idx]) == 0)
-		{   
-        	if (important(&node, final, &temp) == 0)
+		{
+			if (important(&node, final, &temp) == 0)
 				return (0);
 		}
 		if (finale(&node, final, &temp) == 0)
