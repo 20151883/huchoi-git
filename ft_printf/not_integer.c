ee7eda72f9eca3b7b2a3221cf8222171eb6c203f
20151883 <nyz1058@nate.com>
2021-02-06 00:10:09 +0900

    origin/master

--- ft_printf/not_integer.c
+++ ft_printf/not_integer.c
@@ -80,8 +80,8 @@ char *case_p(t_node *p_node, char **line)
 	p_node->count += lenth;
 	if (0 == (ret = (char *)ft_calloc(sizeof(char), (lenth + 1))))
 		return (0);
-	//if (p_node->p_p == 0)
-	//	return (man_zero(p_node, line, &ret, lenth));
+	if (p_node->p_p == 0)
+		return (man_zero(p_node, line, &ret, lenth));
 	if (ft_strchr(line[1], '-'))//0 flag is not used...?00...
 	{
 		ft_strlcpy(ret, save, size + 1);
