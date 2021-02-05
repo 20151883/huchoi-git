1616175422d4de765652fbe1211f5505af6a98e2
20151883 <nyz1058@nate.com>
2021-02-06 01:31:13 +0900

    origin/master

--- ft_printf/not_integer.c
+++ ft_printf/not_integer.c
@@ -78,6 +78,10 @@ char *case_p(t_node *p_node, char **line)
 	lenth = size;
 	if (atoi_positive(line[2]) > size)
 		lenth = atoi_positive(line[2]);
+	if (p_node->p_p == 0 && ft_atoi(line[2]) == 0 && p_node->is_precision == 1)
+		lenth = 2;
+	else if (p_node->p_p == 0)
+		lenth = 3;
 	p_node->count += lenth;
 	//printf("lenth : %d", lenth);
 	if (0 == (ret = (char *)ft_calloc(sizeof(char), (lenth + 1))))
