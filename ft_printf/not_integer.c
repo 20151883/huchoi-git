635ef54bbce3c048838e8b78d7f6247680f525c7
20151883 <nyz1058@nate.com>
2021-02-06 00:00:39 +0900

    origin/master

--- ft_printf/not_integer.c
+++ ft_printf/not_integer.c
@@ -104,18 +104,18 @@ char *man_zero(t_node *p_node, char **line, char **p_ret, int lenth)
 		{
 			ft_strlcpy(*p_ret, "0x", 3);
 			if (lenth != 2)
-				ret[2] =  ' ';
+				*p_ret[2] =  ' ';
 		}
 		else
-			ft_strlcpy(&ret[lenth - 2], "0x", 3);
+			ft_strlcpy(&(*p_ret[lenth - 2]), "0x", 3);
 	}
 	else
 	{
 		if (ft_strchr(line[2], '-'))
 		{
-			ft_strlcpy(*p_ret, "0x0", 4)
+			ft_strlcpy(*p_ret, "0x0", 4);
 			if (lenth != 3)
-				ret[2] =  ' ';
+				*p_ret[2] =  ' ';
 		}
 		else
 			ft_strlcpy(*p_ret[lenth - 3], "0x0", 4);
