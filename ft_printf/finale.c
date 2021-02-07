ee052da61b22c90c6c6b15064a8a8b121a9732e4
Hunjin Choi <huchoi@c4r3s6.42seoul.kr>
2021-02-07 17:02:28 +0900

    origin/master

--- ft_printf/finale.c
+++ ft_printf/finale.c
@@ -1,21 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   finale.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: huchoi <huchoi@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/07 16:14:50 by huchoi            #+#    #+#             */
+/*   Updated: 2021/02/07 16:25:10 by huchoi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "./includes/ft_printf.h"
 
-int finale(t_node *p_node, char **line, char **p_temp)
+int		finale(t_node *p_node, char **line, char **p_temp)
 {
-    char *str;
+	char	*str;
 
-    str = p_node->string;
-    type_case_sort(p_node);
-    if (!complete_final(line, &(p_node->my_case), p_temp))
-        return (0);
-    if (!inter_type(line, p_temp, p_node, str[p_node->idx]))//p_node, str[p_node->idx]))
-        return (0);     
-    if (!real_complete_final(p_node, line, p_temp))
-        return (0);
-    return (1);
+	str = p_node->string;
+	type_case_sort(p_node);
+	if (!complete_final(line, &(p_node->my_case), p_temp))
+		return (0);
+	if (!inter_type(line, p_temp, p_node, str[p_node->idx]))
+		return (0);
+	if (!real_complete_final(p_node, line, p_temp))
+		return (0);
+	return (1);
 }
 
-void type_case_sort(t_node *p_node)
+void	type_case_sort(t_node *p_node)
 {
 	char *str;
 
@@ -27,68 +39,66 @@ void type_case_sort(t_node *p_node)
 		if (str[p_node->idx] == 'c')
 			p_node->for_c = (char)va_arg(g_ap, int);
 		else if (str[p_node->idx] == 's')
-			p_node->for_s = va_arg(g_ap , char *);//
-        else if (str[p_node->idx] == 'p')
-            p_node->p_p = va_arg(g_ap, unsigned long long);
-        else if (ft_strchr("di", str[p_node->idx]) != 0)
-            p_node->yes_int = va_arg(g_ap, int);
-        else
-            p_node->un_int = va_arg(g_ap, unsigned int);
+			p_node->for_s = va_arg(g_ap, char *);
+		else if (str[p_node->idx] == 'p')
+			p_node->p_p = va_arg(g_ap, unsigned long long);
+		else if (ft_strchr("di", str[p_node->idx]) != 0)
+			p_node->yes_int = va_arg(g_ap, int);
+		else
+			p_node->un_int = va_arg(g_ap, unsigned int);
 	}
 }
 
-
-int complete_final(char **line, int *p_my_case, char **p_temp)
+int		complete_final(char **line, int *p_my_case, char **p_temp)
 {
-    if (0 == (line[*p_my_case] = ft_strdup(*p_temp)))
-        return (free_ret_zero(line, *p_temp, 0, *p_my_case));
-    (*p_my_case)++;
-    while (*p_my_case <= 5)
-    {
-        if (0 == (line[*p_my_case] = ft_strdup("")))
-            return (free_ret_zero(line, *p_temp, 0, *p_my_case));
-        (*p_my_case)++;
-    }
-    ft_memset(*p_temp, '\0', ft_strlen(*p_temp) + 1);
-    **p_temp = '\0';
-    return (1);
+	if (0 == (line[*p_my_case] = ft_strdup(*p_temp)))
+		return (free_ret_zero(line, *p_temp, 0, *p_my_case));
+	(*p_my_case)++;
+	while (*p_my_case <= 5)
+	{
+		if (0 == (line[*p_my_case] = ft_strdup("")))
+			return (free_ret_zero(line, *p_temp, 0, *p_my_case));
+		(*p_my_case)++;
+	}
+	ft_memset(*p_temp, '\0', ft_strlen(*p_temp) + 1);
+	**p_temp = '\0';
+	return (1);
 }
 
-int inter_type(char **line, char **p_temp, t_node *p_node, char the_type)
+int		inter_type(char **line, char **p_temp, t_node *p_node, char the_type)
 {
-    if (the_type == 'c' || the_type == '%')
-        return (letter_c_percent(line, p_temp, p_node->for_c));
-    if (the_type == 's')
-        return (letter_s(line, p_temp, p_node->for_s));
-    if (the_type == 'p')
-        return (letter_p(line, p_temp, p_node->p_p));
-    if (ft_strchr("di", the_type) != 0)
-        return (letter_di(line, p_temp, p_node->yes_int));
-    if (ft_strchr("uxX", the_type) != 0)
-        return (letter_uxX(line, p_temp, p_node->un_int, the_type));
-    else
-        return (free_ret_zero(line, *p_temp, NULL, 5));
+	if (the_type == 'c' || the_type == '%')
+		return (letter_c_percent(line, p_temp, p_node->for_c));
+	if (the_type == 's')
+		return (letter_s(line, p_temp, p_node->for_s));
+	if (the_type == 'p')
+		return (letter_p(line, p_temp, p_node->p_p));
+	if (ft_strchr("di", the_type) != 0)
+		return (letter_di(line, p_temp, p_node->yes_int));
+	if (ft_strchr("uxX", the_type) != 0)
+		return (letter_uxX(line, p_temp, p_node->un_int, the_type));
+	else
+		return (free_ret_zero(line, *p_temp, NULL, 5));
 }
 
-
-int real_complete_final(t_node *p_node, char **line, char**p_temp)
+int		real_complete_final(t_node *p_node, char **line, char **p_temp)
 {
-    char *arr;
-    char *str;
-    int i;
+	char	*arr;
+	char	*str;
+	int		i;
 
-    str = p_node->string;
-    i = p_node->idx;
-    if (0 == (line[5] = ft_strdup(*p_temp)))
-        return (free_ret_zero(line, *p_temp, NULL, p_node->my_case));
-    if (0 == (arr = make_string(p_node, line,p_temp, str[i])))
-        return (free_ret_zero(line, *p_temp, 0, 6));//free is not already done..! 
-    ft_putstr_fd(arr, 1);
-    free_ret_zero(line, *p_temp, arr, 6);
-    (p_node->idx)++;
-    p_node->my_case = 1;
-    p_node->is_precision = 0;
-    p_node->buf = 0;
-    p_node->two_star = 0;
-    return (1);
+	str = p_node->string;
+	i = p_node->idx;
+	if (0 == (line[5] = ft_strdup(*p_temp)))
+		return (free_ret_zero(line, *p_temp, NULL, p_node->my_case));
+	if (0 == (arr = make_string(p_node, line, p_temp, str[i])))
+		return (free_ret_zero(line, *p_temp, 0, 6));
+	ft_putstr_fd(arr, 1);
+	free_ret_zero(line, *p_temp, arr, 6);
+	(p_node->idx)++;
+	p_node->my_case = 1;
+	p_node->is_precision = 0;
+	p_node->buf = 0;
+	p_node->two_star = 0;
+	return (1);
 }
--- ft_printf/integer_signed.c
+++ ft_printf/integer_signed.c
@@ -1,26 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   integer_signed.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: huchoi <huchoi@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/07 16:33:17 by huchoi            #+#    #+#             */
+/*   Updated: 2021/02/07 16:38:02 by huchoi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "./includes/ft_printf.h"
 
-char *case_d(t_node *p_node, char **line)
+char	*case_d(t_node *p_node, char **line)
 {
-    int size;
-    int plus;
-    int lenth;
-    char *ret;
+	int		size;
+	int		plus;
+	int		lenth;
+	char	*ret;
 
-    minus_express(line, &size, &plus, &lenth);
-     if (p_node->is_precision == 1)
-        check_zero(line, &size, &plus, &lenth);
-    if (*line[2] != '\0' && lenth < atoi_positive(line[2]))
-        lenth = atoi_positive(line[2]);
-    (p_node->count) += lenth;
-    if (p_node->is_precision == 1 && ft_atoi(line[3]) >= 0)
-        ret = precision_int(line, plus, size, lenth);
-    else
-        ret = no_precision_int(line, size, lenth);    
-    return (ret);
+	minus_express(line, &size, &plus, &lenth);
+	if (p_node->is_precision == 1)
+		check_zero(line, &size, &plus, &lenth);
+	if (*line[2] != '\0' && lenth < atoi_positive(line[2]))
+		lenth = atoi_positive(line[2]);
+	(p_node->count) += lenth;
+	if (p_node->is_precision == 1 && ft_atoi(line[3]) >= 0)
+		ret = precision_int(line, plus, size, lenth);
+	else
+		ret = no_precision_int(line, size, lenth);
+	return (ret);
 }
 
-int check_zero(char **line, int *p1, int *p2, int *p3)
+int		check_zero(char **line, int *p1, int *p2, int *p3)
 {
 	if (ft_atoi(line[5]) == 0 && ft_atoi(line[3]) == 0)
 	{
@@ -33,42 +45,42 @@ int check_zero(char **line, int *p1, int *p2, int *p3)
 		return (0);
 }
 
-void minus_express(char **line, int *p_size, int *p_plus, int *p_lenth)
+void	minus_express(char **line, int *p_size, int *p_plus, int *p_lenth)
 {
-    *p_size = ft_strlen(line[5]);
-    if (ft_atoi(line[5]) < 0)
-    {
-        if (ft_atoi(line[3]) > (*p_size - 1))
-            *p_plus = ft_atoi(line[3]) - (*p_size - 1);
-        else
-            *p_plus = 0;
-    }
-    else
-    {
-        if (*line[3] != '\0' && *p_size < atoi(line[3]))
-            *p_plus = atoi(line[3]) - *p_size;
-        else
-            *p_plus = 0;
-    }
-    *p_lenth = *p_size + *p_plus;
+	*p_size = ft_strlen(line[5]);
+	if (ft_atoi(line[5]) < 0)
+	{
+		if (ft_atoi(line[3]) > (*p_size - 1))
+			*p_plus = ft_atoi(line[3]) - (*p_size - 1);
+		else
+			*p_plus = 0;
+	}
+	else
+	{
+		if (*line[3] != '\0' && *p_size < atoi(line[3]))
+			*p_plus = atoi(line[3]) - *p_size;
+		else
+			*p_plus = 0;
+	}
+	*p_lenth = *p_size + *p_plus;
 }
 
-char *case_i(t_node *p_node, char **line)
+char	*case_i(t_node *p_node, char **line)
 {
-    int size;
-    int plus;
-    int lenth;
-    char *ret;
+	int		size;
+	int		plus;
+	int		lenth;
+	char	*ret;
 
-    minus_express(line, &size, &plus, &lenth);
-    if (p_node->is_precision == 1)
-        check_zero(line, &size, &plus, &lenth);
-    if (*line[2] != '\0' && lenth < atoi_positive(line[2]))
-        lenth = atoi_positive(line[2]);
-    (p_node->count) += lenth;
-    if (p_node->is_precision == 1 && ft_atoi(line[3]) >= 0)
-        ret = precision_int(line, plus, size, lenth);
-    else
-        ret = no_precision_int(line, size, lenth);    
-    return (ret);
+	minus_express(line, &size, &plus, &lenth);
+	if (p_node->is_precision == 1)
+		check_zero(line, &size, &plus, &lenth);
+	if (*line[2] != '\0' && lenth < atoi_positive(line[2]))
+		lenth = atoi_positive(line[2]);
+	(p_node->count) += lenth;
+	if (p_node->is_precision == 1 && ft_atoi(line[3]) >= 0)
+		ret = precision_int(line, plus, size, lenth);
+	else
+		ret = no_precision_int(line, size, lenth);
+	return (ret);
 }
