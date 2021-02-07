d41dcc8f49ade92e0c328218cf0c89a152f7ad6a
20151883 <nyz1058@nate.com>
2021-02-07 15:26:05 +0900

    origin/master

--- ft_printf/includes/ft_printf.h
+++ ft_printf/includes/ft_printf.h
@@ -35,8 +35,8 @@ typedef struct					s_node
 		int						two_star;
 		int						is_precision;
 }								t_node;
-int 	define_lenth(t_node *p_node,char **line, int size);
 void	make_0x(char **line, char *ret, int lenth);
+int		define_lenth(t_node *p_node, char **line, int size);
 void	make_0x0(char **line, char *ret, int lenth);
 char	*intger_value_is_zero(unsigned int num);
 char	*man_zero(t_node *p_node, char **line, char **p_ret, int lenth);
@@ -63,7 +63,8 @@ void	init_node(t_node* p_node, const char *str);
 int		contact_with_format(t_node *p_node, char **line, char **p_temp);
 int		real_complete_final(t_node *p_node, char **line, char**p_temp);
 char	*ten_unsigned(unsigned int num);
-int		letter_uxX(char **line, char **p_temp, unsigned int value, char the_tpye);
+int		letter_uxX(char **line, char **p_temp, unsigned int value, \
+		char the_tpye);
 int		letter_di(char **line, char **p_temp, int value);
 int		letter_p(char **line, char **p_temp, unsigned long long value);
 int		letter_s(char **line, char **p_temp, char *value);
@@ -72,8 +73,10 @@ int		inter_type(char **line, char **p_temp, t_node *p_node, char the_type);
 int		complete_final(char **line, int *p_my_case, char **p_temp);
 int		flag_precise(char **line, char **p_temp, t_node *p_node);
 int		case_changed(char **line, char **p_temp, int *p_my_case);
-int		star(char **line, char **p_temp, char *buf, int my_case, t_node *p_node);
-int		not_star(char **line, char *str, char **p_temp, t_node *p_node);
+int		star(char **line, char **p_temp, char *buf, int my_case, \
+		t_node *p_node);
+int		not_star(char **line, char *str, char **p_temp, \
+		t_node *p_node);
 int		free_ret_zero(char **p, char *s1, char *s2, int my_case);
 char	*precision_int(char **line, int plus, int size, int lenth);
 char	*precision_unsigned(char **line, int plus, int size, int lenth);
@@ -83,7 +86,8 @@ char	*ten_big_hex(unsigned int num);
 char	*ten_small_hex(unsigned int num);
 int		ft_printf(const char *str, ...);
 int		what_my_case(char c, int now_case);
-char	*make_string(t_node *p_node, char **final, char **p_tmep, char the_type);
+char	*make_string(t_node *p_node, char **final, char **p_tmep, \
+		char the_type);
 char	*case_c(t_node *p_node, char **final);
 char	*case_s(t_node *p_node, char **final, char **p_temp);
 char	*case_p(t_node *p_node, char **final);
