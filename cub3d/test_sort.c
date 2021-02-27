2b67a34795b7467c972edf3920a3af576a7f7db0
20151883 <nyz1058@nate.com>
2021-02-28 05:14:30 +0900

    origin/master

--- cub3d/test_sort.c
+++ cub3d/test_sort.c
@@ -10,7 +10,7 @@ void ft_merge(double *arr, int start, int mid, int end)
     while (left <= mid && right <= end)
     {
         if (arr[left] < arr[right])
-            temp[idx] = arr[left++]
+            temp[idx] = arr[left++];
         else
             temp[idx] = arr[right++];
         idx++;
@@ -49,5 +49,7 @@ int main(void)
 {
     double arr[10] = {9,2,0,1,5,6,7,8,4,3};
     ft_mergesort(arr, 0, 9);
-    printf("%d", arr[0]);
+    int i = 0;
+    while (i < 10)
+        printf("%l", arr[i]);
 }
