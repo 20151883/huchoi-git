076a0b6ee8ae44d62d28b0e5dd5f520051d4cc09
huchoi <nyz1058@nate.com>
2021-03-01 02:04:55 +0900

    origin/master

--- cub3d/tri_to_DDA.c
+++ cub3d/tri_to_DDA.c
@@ -1,44 +1,38 @@
 #include "cub3d.h"
 
-void change_uclied_vertical(t_tri *p_tri)
+void	change_uclied_vertical(t_tri *p_tri)
 {
-    t_dda *p_dd;
+	t_dda *p_dd;
 
-    p_dd = &(p_tri->dda);
-    //공식을 이용하시오...!!!
-     if (p_tri->dda.side == 0) 
-        p_dd->walldist = (p_dd->map_x - p_tri->pos[0] + (1 - p_dd->step_x) / 2) / (p_dd->raydir_x);
-    else
-        p_dd->walldist = (p_dd->map_y - p_tri->pos[1] + (1 - p_dd->step_y) / 2) / (p_dd->raydir_y);
+	p_dd = &(p_tri->dda);
+	if (p_tri->dda.side == 0) 
+		p_dd->walldist = (p_dd->map_x - p_tri->pos[0] + (1 - p_dd->step_x) / 2) / (p_dd->raydir_x);
+	else
+		p_dd->walldist = (p_dd->map_y - p_tri->pos[1] + (1 - p_dd->step_y) / 2) / (p_dd->raydir_y);
 }
 
-void dda_loop(t_tri *p_tri)//&(p_tri->dda) = p_dd
+void	dda_loop(t_tri *p_tri)//&(p_tri->dda) = p_dd
 {
-    t_dda *p_dd;
-    int hit = 0;
+	t_dda	*p_dd;
+	int		hit;
 
-    p_dd = &(p_tri->dda);
-    int test = 10;
-    while (hit == 0)
-    {
-        //printf("IN DDA \n pos[0] = %f pos[1] = %f  vvvv: %d", p_tri->pos[0], p_tri->pos[1], p_tri->test_map[p_tri->dda.map_x][p_tri->dda.map_y]);
-        if (p_dd->sidedist_x <= p_dd->sidedist_y)
-        {
-            p_dd->sidedist_x += p_dd->deltadist_x;
-            p_dd->side = 0;
-            p_dd->map_x += p_dd->step_x; 
-        }
-        else
-        {
-            p_dd->sidedist_y += p_dd->deltadist_y;
-            p_dd->side = 1;
-            p_dd->map_y += p_dd->step_y; 
-        }
-        //printf("here %d %d value : %d\n", p_dd->map_x, p_dd->map_y, p_tri->test_map[p_tri->dda.map_x][p_tri->dda.map_y]);
-        if (p_tri->test_map[p_dd->map_x][p_dd->map_y] == 1)
-        {
-            hit = 1;
-            //printf("asdasdasfasdfsdgds\n");
-        }
+	hit = 0;
+	p_dd = &(p_tri->dda);
+	while (hit == 0)
+	{
+		if (p_dd->sidedist_x <= p_dd->sidedist_y)
+		{
+			p_dd->sidedist_x += p_dd->deltadist_x;
+			p_dd->side = 0;
+			p_dd->map_x += p_dd->step_x; 
+		}
+		else
+		{
+			p_dd->sidedist_y += p_dd->deltadist_y;
+			p_dd->side = 1;
+			p_dd->map_y += p_dd->step_y; 
+		}
+		if (p_tri->test_map[p_dd->map_x][p_dd->map_y] == 1)
+			hit = 1;
     }
 }
