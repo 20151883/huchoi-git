512f0c0ffefcdcc5e0e7d8cdd5ff147d746fab69
Hunjin Choi <huchoi@c3r2s2.42seoul.kr>
2021-03-08 23:17:17 +0900

    origin/master

--- exam01/gnl/gnl.c
+++ exam01/gnl/gnl.c
@@ -1,3 +1,25 @@
-#include <gnl.h>
+#include "gnl.h"
 
+#define BUFFER_SIZE 100;
 
+int	get_next_line(int fd, char **line)
+{
+	static char *backup;
+	char *buf;
+	int ret;
+	buf = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	while(1)
+	{
+		ret = read(fd, buf, BUFFER_SIZE);
+		buf[ret] = 0;
+		if(ret == 0)
+			return (case_zero(line, backup, buf));
+		else if (0 < ret && ret <= BUFFER_SIZE)
+		{
+			ret = case_non_zero(line, backup, buf);
+			if (ret == 42)
+				continue;
+			return (ret);
+		}
+	}
+}
---
+++ exam01/test_pritnf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int count = printf("%10s",NULL);
+	printf("%d\n",printf("%s", NULL));
+	return (0);
+}
