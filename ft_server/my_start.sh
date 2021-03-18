a43cdc73aef7a89953366a803bd87c20a62f4250
huchoi <nyz1058@nate.com>
2021-03-18 00:44:19 +0000

    origin/master

--- ft_server/my_start.sh
+++
@@ -1,4 +0,0 @@
-bash nginx restart
-bash php7.3-fpm restart
-bash mysql < var/www/html/phpmyadmin/sql/create_tables.sql
-BASH
---
+++ ft_server/srcs/my_start.sh
@@ -0,0 +1,4 @@
+nginx restart
+php7.3-fpm restart
+##mysql < var/www/html/phpmyadmin/sql/create_tables.sql
+BASH
