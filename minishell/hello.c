effe1ce4bc2b70903b65d778fdcb57fd5aac39b9
Hunjin Choi <huchoi@c3r1s1.42seoul.kr>
2021-04-06 21:31:55 +0900

    origin/master

--- minishell/hello.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-#include <stdio.h>
-
-int main(void)
-{
-    printf("hello");
-    write(1, "world", 5);
-    return (0);
-}
---
+++ minishell/pwd.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+
+char *my_read()
+{
+    char *ret;
+    char buf[5];
+    int readsize;
+    while (1)
+    {
+        readsize = read(1, buf, 4);
+        if (buf[readsize - 1] == EOF)
+    }
+}
+int main(void)
+{
+    write(1, "MINISHELL>>", 11);
+    while (1)
+    {
+        
+        write(1, "MINISHELL>>", 11);
+    }
+    return (0);
+}
