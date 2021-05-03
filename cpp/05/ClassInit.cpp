e88b2247fc85346186817d65d920b5dabe7bfdb6
Hunjin Choi <huchoi@c6r6s6.42seoul.kr>
2021-05-03 10:47:48 +0900

    study cpp ch 05

--- cpp/05/ClassInit.cpp
+++ cpp/05/ClassInit.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
-
+//복사 생성자에 대해서...
+//객체생성시 동일종류의 객체를 인자로 넣어주면, 해당 인자를 받는 생성자(생성자는 함수이다)가 호출될것...
 class SoSimple
 {
 private:
@@ -29,9 +30,9 @@ public:
 int main(void)
 {
 	SoSimple sim1(15, 30);
-	cout<<"���� �� �ʱ�ȭ ����"<<endl;
+	cout<<"생성및 초기화 직전"<<endl;
 	SoSimple sim2=sim1;
-	cout<<"���� �� �ʱ�ȭ ����"<<endl;
+	cout<<"생성및 초기화 직후"<<endl;
 	sim2.ShowSimpleData();
 	return 0;
 }
