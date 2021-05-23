0249d6819a71452737f760c11cd032f5f2fc17eb
huchoi <nyz1058@nate.com>
2021-05-23 19:54:48 +0900

    origin master

--- study_web/TCPIP_Src/05*/echo_client2*.c
+++ study_web/TCPIP_Src/05*/echo_client2*.c
@@ -19,44 +19,46 @@ int main(int argc, char *argv[])
 		printf("Usage : %s <IP> <port>\n", argv[0]);
 		exit(1);
 	}
-	
-	sock=socket(PF_INET, SOCK_STREAM, 0);   
+
+	sock=socket(PF_INET, SOCK_STREAM, 0);
 	if(sock==-1)
 		error_handling("socket() error");
-	
+
 	memset(&serv_adr, 0, sizeof(serv_adr));
 	serv_adr.sin_family=AF_INET;
 	serv_adr.sin_addr.s_addr=inet_addr(argv[1]);
 	serv_adr.sin_port=htons(atoi(argv[2]));
-	
+
 	if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
 		error_handling("connect() error!");
 	else
 		puts("Connected...........");
-	
-	while(1) 
+
+	while(1)
 	{
 		fputs("Input message(Q to quit): ", stdout);
 		fgets(message, BUF_SIZE, stdin);
-		
+
 		if(!strcmp(message,"q\n") || !strcmp(message,"Q\n"))
 			break;
 
 		str_len=write(sock, message, strlen(message));
-		
+
 		recv_len=0;
-		while(recv_len<str_len)
+		while(recv_len<str_len)//아래의 반복문과는 구별을 해야함.
 		{
 			recv_cnt=read(sock, &message[recv_len], BUF_SIZE-1);
 			if(recv_cnt==-1)
 				error_handling("read() error!");
 			recv_len+=recv_cnt;
 		}
-		
+		/*while (read(sock, &message[recv_len], BUF_SIZE-1) != 0)
+		{}
+		*/
 		message[recv_len]=0;
 		printf("Message from server: %s", message);
 	}
-	
+
 	close(sock);
 	return 0;
 }
