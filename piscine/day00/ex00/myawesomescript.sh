ffc06a7f38a47a10a3ca52eeb5cfa479c1c7a5b9
huchoi <nyz1058@nate.com>
2021-05-26 23:39:35 +0900

    asdas

--- piscine/day00/ex00/myawesomescript.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#if $# != 1 then echo "error" && return -1
-curl -l $1 2>>/dev/null | grep "href=" | cut -f 2 -d \"
--- piscine/day01/ex00/var.rb
+++ piscine/day01/ex00/var.rb
@@ -1,9 +1,9 @@
+#!/usr/bin/ruby
 def my_var
 	var1 = "test var"
 	var2 = 3.5
 	var3 = 3
 	var4 = nil
-	p "test : #{10.class}"
 	puts "var1 contains: #{var1} and is a type: #{var1.class}"
 	puts "var2 contains: #{var2} and is a type: #{var2.class}"
 	puts "var3 contains: #{var3} and is a type: #{var3.class}"
