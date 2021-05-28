877faa3ea14902e284d6c7c540292bf5ce295f6e
huchoi <nyz1058@nate.com>
2021-05-28 13:04:06 +0900

    origin master

--- piscine/day03/ex01/test_library.rb
+++ piscine/day03/ex01/test_library.rb
@@ -12,11 +12,13 @@ class Ft_wikipedia
 	print "first @ :" if @searched_keyword.empty?
 	puts "#{link}"
 	doc = Nokogiri::HTML(URI.open(link))
-	raise StandardError, "ead search #{@link}" if !doc#굳이 raise 발생 안시켜도 됨
+	#raise StandardError, "ead search #{@link}" if !doc#굳이 raise 발생 안시켜도 됨
+	#data = doc.search('div.mv-parser-output p a')
 	data = doc.search('div p a')
-	raise StandardError, "Dead search #{@link}" if !data#굳이 raise 발생 안시켜도 됨
+	#raise StandardError, "Dead search #{@link}" if !data#굳이 raise 발생 안시켜도 됨
 
 	first = nil
+	raise StandardError, "nodata" if data.empty?
 	data.each do |var|
 		first = var if var.to_s.include?("/wiki")
 		break if first != nil
@@ -26,20 +28,23 @@ class Ft_wikipedia
       @parse = x if x.include?("wiki")
 	end
 	search_value = @parse.split("/")[2] if @parse.is_a?(String)
-	return if @searched_keyword.include?(search_value)
+	raise StandardError, "loop" if @searched_keyword.include?(search_value)
 	@searched_keyword << search_value
 	puts @searched_keyword[-1]
     @count += 1
     search("#{@searched_keyword[-1]}")
 	#puts @count
 
-	rescue StandardError => var
+	rescue OpenURI::HTTPError => var
+		puts var.class
+		puts var.message
+	rescue StandardError=>var
 		puts var.message
 	end
   end
 end
 a = Ft_wikipedia.new
 #puts a.class
-a.search("asdasdasfawqfawfcassc")
+a.search("User:Thangmayducminh")
 
 
